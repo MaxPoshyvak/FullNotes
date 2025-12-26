0960c67e865b18f341c9e6f323bd11f8f7f0882c
Max <kvsam7395@gmail.com>
2025-12-26 16:20:25 +0200

    add helmet to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,29 @@
 import express from 'express';
 import router from './routes';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+            'script-src': [
+                "'self'",
+                "'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo='",
+                "'sha256-6LU9PSbLOz3Fut6nSXIUTAR/I8FA+yDUlMq4ITGptFo='",
+            ],
+        },
+    })
+);
 app.use(
     cors({
         origin: 'http://localhost:3000',
         credentials: true,
     })
 );
+
 app.use(express.json());
 app.use('/api', router);
 
