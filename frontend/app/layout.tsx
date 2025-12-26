d948cb7af42555df710dfa2c2148794fadecd368
Max <kvsam7395@gmail.com>
2025-12-26 16:40:27 +0200

    bug fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <meta http-equiv="Content-Security-Policy" content="..."></meta>
+            </head>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
         </html>
     );
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)', // для всіх сторінок
+                source: '/(.*)',
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; img-src 'self' https://fullnotes.onrender.com; script-src 'self' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo='; style-src 'self';",
+                        // Оновлений рядок з усіма дозволами:
+                        value: "default-src 'self'; connect-src 'self' https://fullnotes.onrender.com https://fullnotes-backend.onrender.com; img-src 'self' data: https://fullnotes.onrender.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo=' 'sha256-6LU9PSbLOz3Fut6nSXIUTAR/I8FA+yDUlMq4ITGptFo=' 'sha256-zSosKOsZVFrezBWW3FR2g7W37YEb3joSvRUwHNbmKrU=' 'sha256-CRBN/fH/CfT4EZGt3lULeOdHJGGaOefA6GHHVE/9Ow4=' 'sha256-xiRz0wX/L2QNFUSelY6FG0GQSHWhuGpD0sAOkVt059c='; style-src 'self' 'unsafe-inline';",
                     },
                 ],
             },
